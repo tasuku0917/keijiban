3091e15b80a9ecc83c319b38405cdb496f5a70c5
tasuku0917 <tasukamesan@outlook.jp>
2021-02-24 16:26:37 +0900

    掲示板　PHP版

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+
+
+ <head>
+  <meta charset="UTF-8">
+     <title>CSS 掲示板</title>
+  <link rel="stylesheet"type="text/css"href="style.css">
+ </head>
+    
+ <body>
+  <img src="4eachblog_logo.jpg">
+  <div class="box">
+    <ul class="menu">
+     <li>トップ</li>
+     <li>プロフィール</li>
+     <li>4eachについて</li>
+     <li>登録フォーム</li>
+     <li>お問い合わせ</li>
+     <li>その他</li>
+    </ul>
+  </div>          
+ </body>
+ 
